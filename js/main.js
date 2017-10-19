f3f68428aa32b553ea6dc6b856248bc056c383f5
Wiktor Koziak <wiktorkoziak@WKMacBook.local>
2017-10-19 21:31:50 +0200

    Added pricing for translations

--- js/main.js
+++ js/main.js
@@ -14,7 +14,8 @@ $( document ).ready( function() {
 	
 	var oldScroll = $( this ).scrollTop();
 	
-	$( document ).scroll( function() {
+	$( window ).scroll( function() {
+		
 		var newScroll = $( this ).scrollTop();
 		
 		if((newScroll > oldScroll) && (oldScroll <= 100) && (newScroll > 100)) {
@@ -110,3 +111,34 @@ $( document ).ready( function() {
 //		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
 //	});
 });
+
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
