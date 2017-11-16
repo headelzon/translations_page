9a66671a6ad8d5362c3d4409a9afac149c68157c
Wiktor Koziak <wiktorkoziak@WKMacBook.local>
2017-11-16 01:03:15 +0100

    Fixed bug with svgs

--- js/main.js
+++ js/main.js
@@ -20,12 +20,12 @@ $( document ).ready( function() {
 		
 		if((newScroll > oldScroll) && (oldScroll <= 100) && (newScroll > 100)) {
 			$( ".navbar" ).addClass("inverted");
-			$( ".navbar .logo" ).attr("data", "./assets/black-logo.svg");
-			$( ".navbar .phone" ).attr("data", "./assets/black-phone.svg");
+			$( "#phone-nav path" ).css("fill", "#000");
+			$( "#logo-nav path" ).css("fill", "#000");
 		} else if((newScroll < oldScroll) && (newScroll <= 100)){
 			$( ".navbar" ).removeClass("inverted");
-			$( ".navbar .logo" ).attr("data", "./assets/white-logo.svg");
-			$( ".navbar .phone" ).attr("data", "./assets/white-phone.svg");
+			$( "#phone-nav path" ).css("fill", "#fff");
+			$( "#logo-nav path" ).css("fill", "#fff");
 		}
 		
 		oldScroll = newScroll;
@@ -36,80 +36,6 @@ $( document ).ready( function() {
 			scrollTop: $( ".translations-categories" ).offset().top
 		}, 800);
 	});
-	
-//	$('#fullpage').fullpage({
-//		//Navigation
-//		menu: '#menu',
-//		lockAnchors: false,
-//		anchors:['firstPage', 'secondPage'],
-//		navigation: false,
-//		navigationPosition: 'right',
-//		navigationTooltips: ['firstSlide', 'secondSlide'],
-//		showActiveTooltip: false,
-//		slidesNavigation: false,
-//		slidesNavPosition: 'bottom',
-//
-//		//Scrolling
-//		css3: true,
-//		scrollingSpeed: 700,
-//		autoScrolling: true,
-//		fitToSection: true,
-//		fitToSectionDelay: 1000,
-//		scrollBar: false,
-//		easing: 'easeInOutCubic',
-//		easingcss3: 'ease',
-//		loopBottom: false,
-//		loopTop: false,
-//		loopHorizontal: true,
-//		continuousVertical: false,
-//		continuousHorizontal: false,
-//		scrollHorizontally: false,
-//		interlockedSlides: false,
-//		dragAndMove: false,
-//		offsetSections: false,
-//		resetSliders: false,
-//		fadingEffect: false,
-//		normalScrollElements: '#element1, .element2',
-//		scrollOverflow: false,
-//		scrollOverflowReset: false,
-//		scrollOverflowOptions: null,
-//		touchSensitivity: 15,
-//		normalScrollElementTouchThreshold: 5,
-//		bigSectionsDestination: null,
-//
-//		//Accessibility
-//		keyboardScrolling: true,
-//		animateAnchor: true,
-//		recordHistory: true,
-//
-//		//Design
-//		controlArrows: true,
-//		verticalCentered: true,
-//		sectionsColor : ['#ccc', '#fff'],
-//		paddingTop: '3em',
-//		paddingBottom: '10px',
-//		fixedElements: '#header, .footer',
-//		responsiveWidth: 0,
-//		responsiveHeight: 0,
-//		responsiveSlides: false,
-//		parallax: false,
-//		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-//
-//		//Custom selectors
-//		sectionSelector: '.section',
-//		slideSelector: '.slide',
-//
-//		lazyLoading: true,
-//
-//		//events
-//		onLeave: function(index, nextIndex, direction){},
-//		afterLoad: function(anchorLink, index){},
-//		afterRender: function(){},
-//		afterResize: function(){},
-//		afterResponsive: function(isResponsive){},
-//		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-//		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-//	});
 });
 
 
